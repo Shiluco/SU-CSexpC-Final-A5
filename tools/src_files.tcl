a9132786a0dd4ad70d6876379193a28d981bbb72
Shiluco <hirap1939@gmail.com>
2026-01-10 11:49:24 +0900

    fix:宣言の不足

--- tools/src_files.tcl
+++ tools/src_files.tcl
@@ -1,12 +1,11 @@
 # Auto-generated. Do not edit.
-# Generated at: 2026-01-08 17:06:17
+# Generated at: 2026-01-10 11:47:25
 
-set_global_assignment -name VERILOG_FILE "SU-CSexpC-Final-A5/src/TOP.v"
 set_global_assignment -name VERILOG_FILE "SU-CSexpC-Final-A5/src/controller/SC.v"
 set_global_assignment -name VERILOG_FILE "SU-CSexpC-Final-A5/src/controller/controller.v"
-set_global_assignment -name VERILOG_FILE "SU-CSexpC-Final-A5/src/controller/isr.v"
 set_global_assignment -name VERILOG_FILE "SU-CSexpC-Final-A5/src/controller/isr_decoder.v"
 set_global_assignment -name VERILOG_FILE "SU-CSexpC-Final-A5/src/lib/itf.v"
 set_global_assignment -name VERILOG_FILE "SU-CSexpC-Final-A5/src/lib/run.v"
 set_global_assignment -name VERILOG_FILE "SU-CSexpC-Final-A5/src/primitives/v_dff_async.v"
 set_global_assignment -name VERILOG_FILE "SU-CSexpC-Final-A5/src/primitives/v_reg16.v"
+set_global_assignment -name VERILOG_FILE "SU-CSexpC-Final-A5/src/top.v"
